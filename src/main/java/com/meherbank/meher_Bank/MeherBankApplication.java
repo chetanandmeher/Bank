8ab8b20d810a698fb4a01be008b4c9f13d06afb1
Chetanand Meher <chetanandmeher1282004@gmail.com>
2024-06-19 12:06:12 +0530

    initial commit

---
+++ src/main/java/com/meherbank/meher_Bank/MeherBankApplication.java
@@ -0,0 +1,13 @@
+package com.meherbank.meher_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeherBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeherBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meherbank/meher_Bank/MeherBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meherbank.meher_Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeherBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
